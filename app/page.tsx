121d493c9271e5561d8c39fe0c2f0f62a4bc2f3a
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2023-10-27 13:36:11 +0200

    add auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from 'next/image'
+import { options } from "./api/auth/[...nextauth]/options"
+import { getServerSession } from "next-auth/next"
+import { redirect } from "next/navigation"
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(options)
+
+  if (!session) {
+    redirect('/api/auth/signin?callbackUrl=/')
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+      <h1 className="text-7xl font-extrabold">Hello World!</h1>
     </main>
   )
 }
