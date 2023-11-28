028f9c924f6b621154c145372020ed447a2931c1
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2023-11-28 11:06:30 +0100

    add come content

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,21 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-7xl font-extrabold">Hello World!</h1>
-    </main>
+    <>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            {/* <UserNav /> */}
+          </div>
+        </div>
+        {/* <DataTable data={tasks} columns={columns} /> */}
+      </div>
+    </>
   )
 }
